61f7feb30bf9eff6e88953b05ab038cd1a0c1c57
LordAntonidus <42327259+LordAntonidus@users.noreply.github.com>
2019-07-31 21:49:08 +1200

    The Roulette Wheel Spins
    IT SPINS I TELL YOU!!!!!!

---
+++ AGH-Roulette/Assets/Scenes/Dev Scene/roulettewheel_spin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roulettewheel_spin : MonoBehaviour
+{
+    public float xAngle, yAngle, zAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
+    }
+}
