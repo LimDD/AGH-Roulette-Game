060db1a6133fe24e2195cae3f1f9f4643f08c262
NghtFrost107 - Laptop <hbn8839@autuni.ac.nz>
2019-08-01 14:42:45 +1200

    Number reader
    Reads audio in strings

--- AGH-Roulette/Assets/Scripts/Example/ExampleNumber.cs
+++ AGH-Roulette/Assets/Scripts/Example/ExampleNumber.cs
@@ -7,25 +7,64 @@ public class ExampleNumber : MonoBehaviour
     public NumberReader numberReader;
 
     [Header("Press A after changing the number to read")]
-    public int NumToRead = 0;
+    public int NumToRead;
 
     AudioSource audioSource;
+    AudioClip[] clips;
+    public int currentClip;
+    public int lastClip;
+    //public AudioSource[] audioSourceArray;
+    //int toggle = 0;
+    //double duration = 2.00;
 
     private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
+        clips = numberReader.GetNumberAudio(NumToRead);
+        currentClip = 0;
+        lastClip = clips.Length;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+            PlayAudio();
+            
+            //for (int i = 0; i < clips.Length; i++)
+            //{
+                //Printing out the clip needed to be played
+                
+                //audioSourceArray[i].clip = clips[i];
+                //audioSourceArray[i].PlayScheduled(AudioSettings.dspTime + getLength(clips[i]));
+
+
+                //toggle = 1 - toggle;
+
+
+                //audioSource.clip = clips[i];
+                //audioSource.Play();
+            //}
+        //}
+
+        //double getLength(AudioClip clip)
+        //{
+        //    double clipLength = (double)clip.samples / clip.frequency;
+
+        //    return clipLength;
+        //}
+    }
+
+    private void PlayAudio()
+    {
+        while (currentClip < lastClip && audioSource.isPlaying == false)
         {
-            AudioClip[] clips = numberReader.GetNumberAudio(NumToRead);
-            for(int i= 0; i < clips.Length; i++)
-            {
-                Debug.Log(clips[i].name);
-            }
+            Debug.Log("Giving " + clips[currentClip].name + " to AS");
+            audioSource.clip = clips[currentClip];
+            audioSource.Play();
+
+            currentClip++;
         }
     }
 }
