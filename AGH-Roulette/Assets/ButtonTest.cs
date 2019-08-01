621d5dc25122bd4383b98c7cc880109747b666c2
Cameron <41931927+fxj6062@users.noreply.github.com>
2019-08-01 12:32:37 +1200

    created a working button

---
+++ AGH-Roulette/Assets/ButtonTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonTest : MonoBehaviour
+{
+    public Button button;
+
+    void Start()
+    {
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    public void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+    }
+}
+
