f1309587f473a5d2b1570cb0ae34f88fd1f0413d
Cameron <41931927+fxj6062@users.noreply.github.com>
2019-08-07 13:32:20 +1200

    updated to add more sounds to the buttons

--- AGH-Roulette/Assets/Scripts/AudioScript.cs
+++ AGH-Roulette/Assets/Scripts/AudioScript.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class AudioScript : MonoBehaviour
 {
+    public AudioClip buttonPress;
     public AudioClip zeroBal;
+    public AudioClip errorSound;
     public AudioSource source;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void Bet()
+    {
+        source.PlayOneShot(buttonPress, 0.7f);
+    }
 
     public void NoCoins()
     {
-
-        source = GetComponent<AudioSource>();
-        source.Play();
-        Debug.Log("yep ur here");
+        source.PlayOneShot(zeroBal, 0.7f);
     }
+
+    public void ErrSound()
+    {
+        source.PlayOneShot(errorSound, 0.7f);
+    }
+
 }
--- AGH-Roulette/Assets/Scripts/BettingScript.cs
+++ AGH-Roulette/Assets/Scripts/BettingScript.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+//In this scenario, the player is losing 1 coin per bet
 public class BettingScript : MonoBehaviour
 {
     public Button button;
@@ -22,17 +24,24 @@ public class BettingScript : MonoBehaviour
     {
         if (coins > 0)
         {
+            aS.Bet();
             coins--;
-            Debug.Log("You have "+coins+" coins left.");
+            //Check if player is now out of coins
+            if (coins == 0)
+            {
+                //This seems to override the button click sound when the player is out of coins
+                aS.NoCoins();
+            }
 
             bal.text = "Balance = " + coins.ToString();
         }
 
         else
         {
-            Debug.Log("Error, You are out of coins");
-            aS.NoCoins();
+            aS.ErrSound();
         }
+
     }
+
 }
 
