78b4d71ead41faecd00022aeb29c6bc7378722ea
Cameron <41931927+fxj6062@users.noreply.github.com>
2019-08-07 14:33:21 +1200

    Text appears on screen when user out of coins
    The text will disappear when the button is clicked again, need a better way to do this

--- AGH-Roulette/Assets/Scripts/BettingScript.cs
+++ AGH-Roulette/Assets/Scripts/BettingScript.cs
@@ -3,25 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 //In this scenario, the player is losing 1 coin per bet
 public class BettingScript : MonoBehaviour
 {
     public Button button;
     public Text bal;
+    public Text noBal;
     public int coins;
     public AudioScript aS;
 
     void Start()
     {
-        coins = 5;
-        bal = GameObject.Find("Canvas/Text").GetComponent<Text>();
-        bal.text = "Balance = "+ coins.ToString();
         aS = FindObjectOfType<AudioScript>();
+
+        coins = 5;
+        bal = GameObject.Find("Canvas/BalText").GetComponent<Text>();
+        bal.text = "Balance = "+ coins.ToString();
+
+        noBal = GameObject.Find("Canvas/noCoinsText").GetComponent<Text>();
+        noBal.gameObject.SetActive(false);
     }
 
     public void OnClick()
     {
+        noBal.gameObject.SetActive(false);
+
         if (coins > 0)
         {
             aS.Bet();
@@ -31,6 +37,8 @@ public class BettingScript : MonoBehaviour
             {
                 //This seems to override the button click sound when the player is out of coins
                 aS.NoCoins();
+                noBal.gameObject.SetActive(true);
+                button.GetComponent<Image>().color = Color.red;
             }
 
             bal.text = "Balance = " + coins.ToString();
@@ -42,6 +50,5 @@ public class BettingScript : MonoBehaviour
         }
 
     }
-
 }
 
