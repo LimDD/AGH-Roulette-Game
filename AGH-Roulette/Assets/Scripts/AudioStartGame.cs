f7ec8a867f8a2311482a0128e03777366a5e8852
LimDD <christian.lim95@hotmail.com>
2019-08-08 13:54:15 +1200

    Audio Added
    Audio added to ONCLICK, needs to be changed to ONSELECT .

---
+++ AGH-Roulette/Assets/Scripts/AudioStartGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    public AudioClip StartGameButton;
+    public AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void StartGame()
+    {
+        source.PlayOneShot(StartGameButton, 0.7f);
+    }
+}
