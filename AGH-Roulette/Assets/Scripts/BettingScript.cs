00c14bb8eb9570939ba7c1888b0aeb738723e5f8
Cameron Laing <fxj6062@autuni.ac.nz>
2019-08-08 12:13:27 +1200

    added background and tried to change some things

--- AGH-Roulette/Assets/Scripts/BettingScript.cs
+++ AGH-Roulette/Assets/Scripts/BettingScript.cs
@@ -20,14 +20,12 @@ public class BettingScript : MonoBehaviour
         bal = GameObject.Find("Canvas/BalText").GetComponent<Text>();
         bal.text = "Balance = "+ coins.ToString();
 
-        noBal = GameObject.Find("Canvas/noCoinsText").GetComponent<Text>();
+        noBal = GameObject.Find("Canvas/NoCoinsText").GetComponent<Text>();
         noBal.gameObject.SetActive(false);
     }
 
     public void OnClick()
     {
-        noBal.gameObject.SetActive(false);
-
         if (coins > 0)
         {
             aS.Bet();
@@ -35,9 +33,9 @@ public class BettingScript : MonoBehaviour
             //Check if player is now out of coins
             if (coins == 0)
             {
-                //This seems to override the button click sound when the player is out of coins
                 aS.NoCoins();
                 noBal.gameObject.SetActive(true);
+                Destroy(noBal, 1.5f);
                 button.GetComponent<Image>().color = Color.red;
             }
 
