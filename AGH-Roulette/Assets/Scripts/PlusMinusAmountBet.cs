d21e03ae85fdc9c754c84f94f322b20845695d85
NghtFrost107 - Laptop <hbn8839@autuni.ac.nz>
2019-08-08 13:48:01 +1200

    Got the Amount bet working just need to copy the values that are needed just the coins

--- AGH-Roulette/Assets/Scripts/PlusMinusAmountBet.cs
+++ AGH-Roulette/Assets/Scripts/PlusMinusAmountBet.cs
@@ -10,33 +10,60 @@ public class PlusMinusAmountBet : MonoBehaviour
     public int amountToChange = 10;
     public int maxbet = 300;
     public int minbet = 10;
-    public Button increaseBet;
-    public Button decreaseBet;
+    //public Button increaseBet;
+    //public Button decreaseBet;
     
 
     public void Increment()
     {
-        int bettingAmount = ConvertText();
+        int bettingAmount = ConvertText(betText);
+
+        bettingAmount += amountToChange;
+
+        ConvertInt(bettingAmount);
 
     }
 
     public void Decrement()
     {    
-        int bettingAmount = ConvertText();
+        int bettingAmount = ConvertText(betText);
+
+        bettingAmount -= amountToChange;
+
+        ConvertInt(bettingAmount);
 
     }
 
-    private int ConvertText()
+    private int ConvertText(Text textToConvert)
     {
         int amountBetting = 0;
-        amountBetting = int.Parse(betText.text);
+        amountBetting = int.Parse(textToConvert.text);
         
         return amountBetting;
     }
 
+    private Text ConvertInt(int intToConvert)
+    {
+        Text convertedInt = betText;
+
+        string text = intToConvert.ToString();
+        convertedInt.text = text;
+        
+        return convertedInt;
+
+    }
     void Update()
     {
         //on button press: call the increase/decrease functions
+        if(Input.GetButtonDown("Increase Bet Button"))
+        {
+            Increment();
+        }
+
+        if(Input.GetButtonDown("Decrease Bet Button"))
+        {
+            Decrement();
+        }
     }
 
 }
