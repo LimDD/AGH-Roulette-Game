b7bc044bdbf4cb3f350c0062590056eb421f69f6
LordAntonidus <42327259+LordAntonidus@users.noreply.github.com>
2019-08-08 21:35:21 +1200

    Spinning Roulette Wheel and Comparing Bets to Roulette Result
    - Wheel has a spin up timer of 60 frames.
    - Wheel has a constant spin speed for 180 frames.
    - Wheel has a spin down timer of 60 frames.
    - Current bets are hardcoded and added to a list.
    - The functions will change the text that shows up at the end of the spin depending on the result.

--- AGH-Roulette/Assets/Scenes/Anton's Scene/roulettewheel_bet.cs
+++ AGH-Roulette/Assets/Scenes/Anton's Scene/roulettewheel_bet.cs
@@ -13,47 +13,21 @@ public class roulettewheel_bet : MonoBehaviour
     
     //Wheel rotation values
     public float xAngle, yAngle, zAngle;
+    public int spintimer;
+    public bool wheelspinning;
 
     //Result Text
-    public GameObject resulttext_object;
-    public Text resulttext_mesh;
+    public Text resulttext_component;
     public string resulttext;
 
     //Betting Values
     public List<int> player_bets;
     public int winning_bet;
 
-    // Start is called before the first frame update
-    void Start()
+    //Check results against hardcoded player bets
+    private void CheckBets()
     {
-        //Result after the wheel spins
-        roulette_value = Random.Range(0, 36);
-
-        //Wheel rotation values
-        xAngle = 0;
-        yAngle = 0;
-        zAngle = -0.75f;
-
-        //Result Text
-        resulttext_object = GameObject.Find("Result Text");
-        resulttext_mesh = resulttext_object.GetComponent<Text>();
-        
-        //Player bets
-        player_bets.Add(7);
-        player_bets.Add(10);
-        player_bets.Add(25);
-        player_bets.Add(32);
-
-        winner = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //Wheel rotation code
-        this.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
-
-        if(winner == false)
+        if (winner == false)
         {
             for (int i = 0; i < (player_bets.Count - 1); i++)
             {
@@ -64,27 +38,92 @@ public class roulettewheel_bet : MonoBehaviour
                 }
             }
         }
-        else if (winner == true)
+
+        CheckIfWinner();
+    }
+
+    //Checks if the result is the same
+    private void CheckIfWinner()
+    {
+        if (winner == true)
         {
             Winner(winning_bet);
         }
         else
         {
-            Loser();
+            Loser(roulette_value);
         }
     }
 
     //Is called when you win a game and the roulette value is the same as your bet
-    public void Winner(int winning_bet)
+    private void Winner(int winning_bet)
     {
-        resulttext = "You won the round with " + winning_bet + "!!!";
-        resulttext_mesh.text = resulttext;
+        resulttext_component.text = "You won the round with " + winning_bet + "!!!";
     }
 
     //Is called when you lose a game and the roulette value is not the same as your bet
-    public void Loser()
+    private void Loser(int roulette_value)
     {
-        resulttext = "You lost the round...";
-        resulttext_mesh.text = resulttext;
+        resulttext_component.text = "The wheel landed on " + roulette_value + " meaning you lost this round...";
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        winner = false;
+
+        //Result after the wheel spins
+        roulette_value = Random.Range(0, 36);
+
+        //Wheel rotation values
+        xAngle = 0;
+        yAngle = 0;
+        zAngle = 0;
+        spintimer = 0;
+        wheelspinning = true;
+
+        //Player bets
+        player_bets.Add(7);
+        player_bets.Add(10);
+        player_bets.Add(25);
+        player_bets.Add(32);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(spintimer < 60)
+        {
+            //Wheel no spinning rotation code
+            this.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
+
+            spintimer += 1;
+            zAngle -= 0.2f;
+        }
+        else if(spintimer == 60 || spintimer < 240)
+        {
+            //Wheel no spinning rotation code
+            this.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
+
+            spintimer += 1;
+        }
+        else if(spintimer == 240 || spintimer < 300)
+        {
+            //Wheel no spinning rotation code
+            this.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
+
+            spintimer += 1;
+            zAngle += 0.2f;
+
+            if(spintimer == 300)
+            {
+                wheelspinning = false;
+            }
+        }
+
+        if(wheelspinning == false)
+        {
+            CheckBets();
+        }
     }
 }
