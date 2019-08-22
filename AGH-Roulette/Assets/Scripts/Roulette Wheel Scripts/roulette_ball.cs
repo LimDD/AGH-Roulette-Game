6753cf457a66f16529b2f0d00c0a03fd118c3f93
LordAntonidus <42327259+LordAntonidus@users.noreply.github.com>
2019-08-22 17:56:38 +1200

    Made a few edits...
    - Changed the Rotation of the Roulette Wheel Screen
    - Added a Sonnar themed Roulette Wheel
    - Added a Roulette Ball to the Scene
    - Realised during development that the existing Roulette Wheel had 38 pockets, switched it out for a 36 pocket wheel.
    - Everything on the Roulette Wheel scene is working fine.

---
+++ AGH-Roulette/Assets/Scripts/Roulette Wheel Scripts/roulette_ball.cs
@@ -0,0 +1,188 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roulette_ball : MonoBehaviour
+{
+    //The Roulette Wheel
+    public GameObject rouletteWheel;
+    public roulettewheel_spin wheelScript;
+
+    //Roulette Wheel result
+    public int rouletteValue;
+
+    //Where the ball starts spinning from
+    public bool startSpin;
+    public float startRotation;
+
+    //Ball rotation values
+    public float xAngle, yAngle, zAngle;
+    
+    //Spin Timer from other script
+    public int spinTimer;
+    public bool wheelSpinning;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rouletteWheel = GameObject.Find("Roulette Wheel");
+        wheelScript = rouletteWheel.GetComponent<roulettewheel_spin>();
+
+        startSpin = true;
+        
+        //Ball rotation values
+        xAngle = 0;
+        yAngle = 0;
+        zAngle = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rouletteValue = wheelScript.rouletteValue;
+        spinTimer = wheelScript.spinTimer;
+        wheelSpinning = wheelScript.wheelSpinning;
+
+        if (startSpin == true && spinTimer == 0)
+        {
+            switch (rouletteValue)
+            {
+                case 0:
+                    startRotation = 190;
+                    break;
+                case 1:
+                    startRotation = 326;
+                    break;
+                case 2:
+                    startRotation = 131;
+                    break;
+                case 3:
+                    startRotation = 209;
+                    break;
+                case 4:
+                    startRotation = 150;
+                    break;
+                case 5:
+                    startRotation = 5;
+                    break;
+                case 6:
+                    startRotation = 92;
+                    break;
+                case 7:
+                    startRotation = 248;
+                    break;
+                case 8:
+                    startRotation = 34;
+                    break;
+                case 9:
+                    startRotation = 287;
+                    break;
+                case 10:
+                    startRotation = 15;
+                    break;
+                case 11:
+                    startRotation = 53;
+                    break;
+                case 12:
+                    startRotation = 228;
+                    break;
+                case 13:
+                    startRotation = 72;
+                    break;
+                case 14:
+                    startRotation = 306;
+                    break;
+                case 15:
+                    startRotation = 170;
+                    break;
+                case 16:
+                    startRotation = 345;
+                    break;
+                case 17:
+                    startRotation = 101;
+                    break;
+                case 18:
+                    startRotation = 267;
+                    break;
+                case 19:
+                    startRotation = 160;
+                    break;
+                case 20:
+                    startRotation = 316;
+                    break;
+                case 21:
+                    startRotation = 140;
+                    break;
+                case 22:
+                    startRotation = 277;
+                    break;
+                case 23:
+                    startRotation = 24;
+                    break;
+                case 24:
+                    startRotation = 355;
+                    break;
+                case 25:
+                    startRotation = 121;
+                    break;
+                case 26:
+                    startRotation = 199;
+                    break;
+                case 27:
+                    startRotation = 101;
+                    break;
+                case 28:
+                    startRotation = 238;
+                    break;
+                case 29:
+                    startRotation = 258;
+                    break;
+                case 30:
+                    startRotation = 43;
+                    break;
+                case 31:
+                    startRotation = 296;
+                    break;
+                case 32:
+                    startRotation = 180;
+                    break;
+                case 33:
+                    startRotation = 335;
+                    break;
+                case 34:
+                    startRotation = 82;
+                    break;
+                case 35:
+                    startRotation = 219;
+                    break;
+                case 36:
+                    startRotation = 62;
+                    break;
+            }
+
+            startSpin = false;
+            this.transform.Rotate(xAngle, yAngle, startRotation, Space.Self);
+        }
+        
+        if (spinTimer < 240)
+        {
+            zAngle = 6.5f;
+
+            //Wheel no spinning rotation code
+            this.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
+        }
+        else if (spinTimer == 240 || spinTimer < 300)
+        {
+            //Wheel no spinning rotation code
+            this.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
+
+            zAngle -= 0.1f;
+        }
+
+        if (wheelSpinning == false)
+        {
+            zAngle = -0.6f;
+            this.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
+        }
+    }
+}
---
+++ AGH-Roulette/Assets/Scripts/Roulette Wheel Scripts/roulettewheel_spin.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class roulettewheel_spin : MonoBehaviour
+{
+    //Result of the bet compared to the roulette wheel value
+    public bool winner;
+
+    //Result after the wheel spins
+    public int rouletteValue;
+        
+    //Wheel rotation values
+    public float xAngle, yAngle, zAngle;
+    public int spinTimer;
+    public bool wheelSpinning;
+
+    //Result Text
+    public Text resulttext_component;
+    public string resulttext;
+
+    //Betting Values
+    public List<int> playerBets;
+    public int winningBet;
+    
+    //Check results against hardcoded player bets
+    private void CheckBets()
+    {
+        if (winner == false)
+        {
+            for (int i = 0; i < (playerBets.Count - 1); i++)
+            {
+                if (playerBets[i] == rouletteValue)
+                {
+                    winningBet = playerBets[i];
+                    winner = true;
+                }
+            }
+        }
+
+        CheckIfWinner();
+    }
+
+    //Checks if the result is the same
+    private void CheckIfWinner()
+    {
+        if (winner == true)
+        {
+            Winner(winningBet);
+        }
+        else
+        {
+            Loser(rouletteValue);
+        }
+    }
+
+    //Is called when you win a game and the roulette value is the same as your bet
+    private void Winner(int winning_bet)
+    {
+        resulttext_component.text = "You won the round with " + winning_bet + "!!!";
+    }
+
+    //Is called when you lose a game and the roulette value is not the same as your bet
+    private void Loser(int roulette_value)
+    {
+        resulttext_component.text = "The ball landed on " + roulette_value + " meaning you lost this round...";
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        winner = false;
+
+        //Result after the wheel spins
+        rouletteValue = Random.Range(0, 36);
+
+        //Wheel rotation values
+        xAngle = 0;
+        yAngle = 0;
+        zAngle = 0;
+        spinTimer = 0;
+        wheelSpinning = true;
+
+        //Player bets
+        playerBets.Add(7);
+        playerBets.Add(10);
+        playerBets.Add(25);
+        playerBets.Add(32);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (wheelSpinning == true)
+        {
+            if(spinTimer < 60)
+            {
+                //Wheel no spinning rotation code
+                this.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
+
+                spinTimer += 1;
+                zAngle -= 0.05f;
+            }
+            else if(spinTimer == 60 || spinTimer < 240)
+            {
+                //Wheel no spinning rotation code
+                this.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
+
+                spinTimer += 1;
+            }
+            else if(spinTimer == 240 || spinTimer < 300)
+            {
+                //Wheel no spinning rotation code
+                this.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
+
+                spinTimer += 1;
+                zAngle += 0.04f;
+
+                if(spinTimer == 300)
+                {
+                    wheelSpinning = false;
+                }
+            }
+        }
+        
+
+        if(wheelSpinning == false)
+        {
+            this.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
+            CheckBets();
+        }
+    }
+}
