96d774584f18f5a67b338223529db49b5b2008c1
Ravmyster1121 <rhninety@gmail.com>
2019-08-26 18:54:50 +1200

    Launch GameScene_Main through GameScene_Menu

---
+++ AGH-Roulette/Assets/BettingController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BettingController : MonoBehaviour
+{
+    int SelectedCell;
+    string Color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetColor()
+    {
+        //TODO: Set the color of the square in the zoom panel.
+    }
+
+    public void SetNumber()
+    {
+        //TODO: Set the number or text of the cell that was tapped on.
+    }
+}
