c8f7ea1bc85ad458ebcf1a4d712abba9b73ca3d8
LordAntonidus <42327259+LordAntonidus@users.noreply.github.com>
2019-08-28 13:48:59 +1200

    COmmented out bit of code that was uncommented
    - Ss.Bet() is commented

---
+++ AGH-Roulette/Assets/Scripts/Corner and Wall Bets.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CornerandWallBets : MonoBehaviour
+{
+    public GameObject PlayerToken;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerToken = GameObject.Find("Player Token");
+
+
+    }
+}
