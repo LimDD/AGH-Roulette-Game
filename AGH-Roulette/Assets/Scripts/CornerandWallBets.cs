aefe1ec40b6625b5c14279eb27850f5dce8fb61e
LordAntonidus <42327259+LordAntonidus@users.noreply.github.com>
2019-08-28 15:10:45 +1200

    Corner and Wall Bets Works
    - Corner Bets Works, values not saved.
    - Wall Bets Works, values not saved.
    - Back Buttons work and makes the corresponding panels deactivate.

---
+++ AGH-Roulette/Assets/Scripts/CornerandWallBets.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CornerandWallBets : MonoBehaviour
+{
+    //public GameObject PlayerToken;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //PlayerToken = GameObject.Find("Player Token");
+    }
+
+    public void MoveUp()
+    {
+        gameObject.transform.Translate(0f, 59.0f, 0f);
+    }
+
+    public void MoveTopRight()
+    {
+        gameObject.transform.Translate(98.0f, 59.0f, 0f);
+    }
+
+    public void MoveRight()
+    {
+        gameObject.transform.Translate(98.0f, 0f, 0f);
+    }
+
+    public void MoveBottomRight()
+    {
+        gameObject.transform.Translate(98.0f, -59.0f, 0f);
+    }
+
+    public void MoveDown()
+    {
+        gameObject.transform.Translate(0f, -59.0f, 0f);
+    }
+
+    public void MoveBottomLeft()
+    {
+        gameObject.transform.Translate(-98.0f, -59.0f, 0f);
+    }
+
+    public void MoveLeft()
+    {
+        gameObject.transform.Translate(-98.0f, 0f, 0f);
+    }
+
+    public void MoveTopLeft()
+    {
+        gameObject.transform.Translate(-98.0f, 59.0f, 0f);
+    }
+}
