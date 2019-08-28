7494c1473aab9f0691aff99abe972027224d71f0
fxj6062 <fxj6062@aut.ac.nz>
2019-08-29 11:33:41 +1200

    more numbers now hidden

--- AGH-Roulette/Assets/Scripts/GetButtonNum.cs
+++ AGH-Roulette/Assets/Scripts/GetButtonNum.cs
@@ -31,13 +31,13 @@ public class GetButtonNum : MonoBehaviour
 
     public void BetType(string num)
     {
-        //topL = GameObject.Find("TopL").GetComponent<Text>();
-        //topM = GameObject.Find("TopM").GetComponent<Text>();
-       // topR = GameObject.Find("TopR").GetComponent<Text>();
-        //midL = GameObject.Find("MiddleL").GetComponent<Text>();
-        //midR = GameObject.Find("MiddleR").GetComponent<Text>();
-        //botL = GameObject.Find("BottomL").GetComponent<Text>();
-        //botM = GameObject.Find("BottomM").GetComponent<Text>();
+        topL = GameObject.Find("TopL").GetComponent<Text>();
+        topM = GameObject.Find("TopM").GetComponent<Text>();
+        topR = GameObject.Find("TopR").GetComponent<Text>();
+        midL = GameObject.Find("MiddleL").GetComponent<Text>();
+        midR = GameObject.Find("MiddleR").GetComponent<Text>();
+        botL = GameObject.Find("BottomL").GetComponent<Text>();
+        botM = GameObject.Find("BottomM").GetComponent<Text>();
         botR = GameObject.Find("BottomR").GetComponent<Text>();
         int n = Convert.ToInt32(num);
 
@@ -45,12 +45,50 @@ public class GetButtonNum : MonoBehaviour
  
         for (int x = 0; x < 11; x++)
         {
-            if (n == 3 * x || n >= 34)
+            if (n == 3 *x)
             {
                 //Commented these out for now as I do not know the best way to restore it after they have completed their bet.
                 botR.GetComponentInParent<Button>().gameObject.SetActive(false);
                 //botR.GetComponentInParent<Button>().interactable = false;
                 x = 12;
+            }
+        }
+
+        if (n <= 3)
+        {
+            topL.GetComponentInParent<Button>().gameObject.SetActive(false);
+            topM.GetComponentInParent<Button>().gameObject.SetActive(false);
+            topR.GetComponentInParent<Button>().gameObject.SetActive(false);
+
+            if (n == 34)
+            {
+                midL.GetComponentInParent<Button>().gameObject.SetActive(false);
+                topL.GetComponentInParent<Button>().gameObject.SetActive(false);
+            }
+
+            if (n == 36)
+            {
+                midR.GetComponentInParent<Button>().gameObject.SetActive(false);
+                topR.GetComponentInParent<Button>().gameObject.SetActive(false);
+            }
+        }
+
+        if (n >= 34)
+        {
+            botL.GetComponentInParent<Button>().gameObject.SetActive(false);
+            botM.GetComponentInParent<Button>().gameObject.SetActive(false);
+            botR.GetComponentInParent<Button>().gameObject.SetActive(false);
+
+            if (n == 34)
+            {
+                midL.GetComponentInParent<Button>().gameObject.SetActive(false);
+                topL.GetComponentInParent<Button>().gameObject.SetActive(false);
+            }
+
+            if (n== 36)
+            {
+                midR.GetComponentInParent<Button>().gameObject.SetActive(false);
+                topR.GetComponentInParent<Button>().gameObject.SetActive(false);
             }
         }
     }
