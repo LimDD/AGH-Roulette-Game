4eda4da315b94bb0449ef6116c53db7c5d9b2999
Cameron <41931927+fxj6062@users.noreply.github.com>
2019-09-07 20:39:34 +1200

    was thinking of saving some stats to be displayed on the stats page
    will add more later

---
+++ AGH-Roulette/Assets/SaveStatistics.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class SaveStatistics : MonoBehaviour
+{
+    private string path = "/statsFile.txt";
+    private List<string> stats = new List<string>();
+
+    public void ReadStats()
+    {
+        int count = 0;
+        StreamReader reader = new StreamReader(Application.persistentDataPath + path);
+
+        while (!reader.EndOfStream)
+        {
+            stats.Add(reader.ReadLine());
+            count++;
+        }
+    }
+
+    public void SaveToFile()
+    {
+        StreamWriter writer = new StreamWriter(Application.persistentDataPath + path);
+    }
+
+}
