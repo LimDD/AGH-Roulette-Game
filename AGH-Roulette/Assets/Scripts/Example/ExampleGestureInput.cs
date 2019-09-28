46498315ed5b92e29235950b38502b84c9d9a583
Cameron <41931927+fxj6062@users.noreply.github.com>
2019-09-28 18:24:51 +1200

    Added gestures to the betting panel
    swipe up and down to change the amount by 100 and left and right to change the amount by 10
    
    double tap to confirm

--- AGH-Roulette/Assets/Scripts/Example/ExampleGestureInput.cs
+++ AGH-Roulette/Assets/Scripts/Example/ExampleGestureInput.cs
@@ -1,15 +1,89 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Text.RegularExpressions;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ExampleGestureInput : MonoBehaviour
 {
+    public GameObject panel;
+    public TMP_Text amount;
+    public TMP_Text balance;
+    public Button confirm;
     // Update is called once per frame
     void Update()
     {
         if (GestureInputManager.CurrentInput != InputAction.Null)
         {
-            Debug.Log(GestureInputManager.CurrentInput);
+            string type = GestureInputManager.CurrentInput.ToString();
+
+            Debug.Log(type);
+
+            if (type == "DoubleClick")
+            {
+                confirm.onClick.Invoke();
+            }
+            
+            if (type.Contains("Swipe"))
+            {
+                if (panel.activeSelf)
+                {
+                    string temp = Regex.Replace(balance.text, "[^.0-9]", "");
+
+                    int am = int.Parse(amount.text);
+
+                    int bal = int.Parse(temp);
+
+                    if (type.Contains("Up"))
+                    {
+                        if (am + 100 <= bal)
+                        {
+                            am += 100;
+                            
+                        }
+
+                        else
+                        {
+                            am = bal;
+                        }
+
+                        amount.text = am.ToString();
+                    }
+
+                    else if (type.Contains("Down"))
+                    {
+                        if (am - 100 >= 10)
+                        {
+                            am -= 100;
+                            
+                        }
+
+                        else
+                        {
+                            am = 10;
+                        }
+
+                        amount.text = am.ToString();
+                    }
+
+                    else if (type.Contains("Left"))
+                    {
+                        if (am - 10 >= 10)
+                        {
+                            am -= 10;
+                            amount.text = am.ToString();
+                        }
+                    }
+
+                    else if (type.Contains("Right"))
+                    {
+                        if (am + 10 <= bal)
+                        {
+                            am += 10;
+                            amount.text = am.ToString();
+                        }
+                    }
+                }
+            }
         }
 
         if (GestureInputManager.CurrentInput == InputAction.Click)
