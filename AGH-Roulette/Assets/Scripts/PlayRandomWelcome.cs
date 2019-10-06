ad7b74173692235128a920da4c9c5e7dbe2addb6
LimDD <christian.lim95@hotmail.com>
2019-10-06 14:46:59 +1300

    Roulette Main And Table
    - Roullette main menu now has 3 greetings randomly come up on every enter
    - Roulette now has 3 greetings randomly  as you enter table

---
+++ AGH-Roulette/Assets/Scripts/PlayRandomWelcome.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayRandomWelcome : MonoBehaviour {
+
+    public AudioSource _as;
+    public AudioClip[] audioClipArray;
+
+    private void Awake()
+    {
+    _as = GetComponent<AudioSource>();
+    }
+    void Start()
+    {
+        _as.clip = audioClipArray[Random.Range(0, audioClipArray.Length)];
+        _as.PlayOneShot(_as.clip);
+    }
+
+}
