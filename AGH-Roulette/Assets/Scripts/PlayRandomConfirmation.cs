0054b053177129d238ba1200621464d434180a80
LordAntonidus <42327259+LordAntonidus@users.noreply.github.com>
2019-10-08 02:22:25 +1300

    Made a few edits...
    - Renamed some audio files.
    - Dealer Confirmation audio has been combined with Make Another Bet audio and is now randomised.
    - Win audio was already randomised.
    - Lose audio was already randomised.
    - There is only one "Congratulation's" audio. No need to randomise.
    - The game is working and I have sent a build to the OneDrive.

---
+++ AGH-Roulette/Assets/Scripts/PlayRandomConfirmation.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayRandomConfirmation : MonoBehaviour
+{
+    public AudioClip confirmationExcellent;
+    public AudioClip confirmationGreat;
+    public AudioClip confirmationPerfect;
+
+    public AudioSource soundManager;
+
+    public bool stateActive;
+    public bool hasPlayed;
+
+    public int audioNum;
+
+    // Awake is called when it is awoken
+    private void Awake()
+    {
+        PlayRandom();
+        Debug.Log("I'M AWAKE");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(stateActive == true)
+        {
+            if(hasPlayed == false)
+            {
+                PlayRandom();
+            }
+        }
+    }
+
+    public void SetHasPlayedTrue()
+    {
+        hasPlayed = true;
+    }
+
+    public void SetHasPlayedFalse()
+    {
+        hasPlayed = false;
+    }
+
+    public void SetStateActiveTrue()
+    {
+        stateActive = true;
+    }
+
+    public void SetStateActiveFalse()
+    {
+        stateActive = false;
+    }
+
+    public void PlayRandom()
+    {
+        audioNum = Random.Range(0, 2);
+
+        if(audioNum == 0)
+        {
+            soundManager.clip = confirmationExcellent;
+            soundManager.Play();
+            SetHasPlayedTrue();
+        }
+        else if(audioNum == 1)
+        {
+            soundManager.clip = confirmationGreat;
+            soundManager.Play();
+            SetHasPlayedTrue();
+        }
+        else if(audioNum == 2)
+        {
+            soundManager.clip = confirmationPerfect;
+            soundManager.Play();
+            SetHasPlayedTrue();
+        }
+    }
+}
