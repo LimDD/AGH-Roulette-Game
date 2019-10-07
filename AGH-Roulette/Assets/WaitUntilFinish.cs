5420125eecd8c911894e560d8f8117a84ad46ad2
Cameron <41931927+fxj6062@users.noreply.github.com>
2019-10-07 20:02:48 +1300

    Fixed sound overlapping in tutorial

---
+++ AGH-Roulette/Assets/WaitUntilFinish.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitUntilFinish : MonoBehaviour
+{
+    public AudioSource source;
+    public AudioSource betReader;
+    void Update()
+    {
+        if (!source.isPlaying)
+        {
+            if (betReader.mute)
+            {
+                betReader.Stop();
+                betReader.mute = false;
+            }
+        }
+    }
+}
