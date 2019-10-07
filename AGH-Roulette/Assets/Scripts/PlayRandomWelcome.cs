e95274589d8ae1bf3cf5e26a9fb1a09c7cea3afa
Cameron <41931927+fxj6062@users.noreply.github.com>
2019-10-07 14:59:39 +1300

    Now audio from buttons doesnt play in menu until greeting has been read

--- AGH-Roulette/Assets/Scripts/PlayRandomWelcome.cs
+++ AGH-Roulette/Assets/Scripts/PlayRandomWelcome.cs
@@ -7,14 +7,37 @@ public class PlayRandomWelcome : MonoBehaviour {
     public AudioSource _as;
     public AudioClip[] audioClipArray;
 
+    public AudioSource sg;
+    public AudioSource t;
+    public AudioSource a;
+    public AudioSource hs;
+    public AudioSource ror;
+    public AudioSource ps;
+
     private void Awake()
     {
-    _as = GetComponent<AudioSource>();
+        _as = GetComponent<AudioSource>();
     }
     void Start()
     {
         _as.clip = audioClipArray[Random.Range(0, audioClipArray.Length)];
         _as.PlayOneShot(_as.clip);
+    }
+
+    private void Update()
+    {
+        if (!_as.isPlaying)
+        {
+            if (sg.mute)
+            {
+                sg.mute = false;
+                t.mute = false;
+                a.mute = false;
+                hs.mute = false;
+                ror.mute = false;
+                ps.mute = false;
+            }
+        }
     }
 
 }
