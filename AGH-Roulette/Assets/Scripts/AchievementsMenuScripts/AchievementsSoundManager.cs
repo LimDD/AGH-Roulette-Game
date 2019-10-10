34412bcfaa48424c384ca186c42908bfc343727e
LordAntonidus <42327259+LordAntonidus@users.noreply.github.com>
2019-10-10 21:40:04 +1300

    Made a few edits...
    - Tidied up the hierarchy for the Achievements menu.
    - Realigned the Achievement titles.
    - RouletteGeek was Beginner and Beginner was RouletteGeek, changed the names of the objects in the hierarchy.
    - Added a Sound Manager to the Achievements Menu.
    - Created Sound Manager Script.
    - Achievements Menu now has sound, you need to hit the text though interestingly enough.

---
+++ AGH-Roulette/Assets/Scripts/AchievementsMenuScripts/AchievementsSoundManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchievementsSoundManager : MonoBehaviour
+{
+    public AudioSource soundManager;
+
+    public AudioClip luckyDay;
+    public AudioClip twoWinStreak;
+    public AudioClip noEvenBets;
+    public AudioClip zero;
+    public AudioClip beginner;
+    public AudioClip rouletteGeek;
+
+    public void PlayLuckyDay()
+    {
+        soundManager.clip = luckyDay;
+        soundManager.Play();
+    }
+
+    public void PlayTwoWinStreak()
+    {
+        soundManager.clip = twoWinStreak;
+        soundManager.Play();
+    }
+
+    public void PlayNoEvenBets()
+    {
+        soundManager.clip = noEvenBets;
+        soundManager.Play();
+    }
+
+    public void PlayZero()
+    {
+        soundManager.clip = zero;
+        soundManager.Play();
+    }
+
+    public void PlayBeginner()
+    {
+        soundManager.clip = beginner;
+        soundManager.Play();
+    }
+
+    public void PlayRouletteGeek()
+    {
+        soundManager.clip = rouletteGeek;
+        soundManager.Play();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
