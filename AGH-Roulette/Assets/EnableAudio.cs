7aa35eec5df4ad04cd5bfdebc1ba791e9a0816e4
Cameron <41931927+fxj6062@users.noreply.github.com>
2019-10-13 18:10:37 +1300

    Now mutes the note being played in the main scene
    The note will not play until narrating is done
    
    Causes a bunch of warnings about null audio source because I couldnt figure out how to disable the script
    
    Will work on adding more tmw

---
+++ AGH-Roulette/Assets/EnableAudio.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableAudio : MonoBehaviour
+{
+    public AudioSource narrator;
+    AudioSource[] sources;
+
+    public GameObject panel;
+    bool enabled;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        sources = panel.GetComponentsInChildren<AudioSource>();
+        enabled = false;
+    }
+
+    void Update()
+    {
+        if (!enabled)
+        {
+            if (narrator.isPlaying)
+            {
+                //Don't play
+            }
+
+            else
+            {
+                foreach (AudioSource s in sources)
+                {
+                    s.enabled = true;
+                }
+                enabled = true;
+            }
+        }
+    }
+
+}
